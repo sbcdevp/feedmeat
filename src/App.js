6dfe38b6950811c35e179fab3f02ee1fdbc57d84
Sergio335566 <bocanceaserge335566@gmail.com>
2019-11-19 14:18:28 +0100

    add lat coords

--- src/App.js
+++ src/App.js
@@ -6,29 +6,32 @@ import HomePage from './components/HomePage';
 import ResultRestaurant from './components/ResultRestaurant'
 // import FontAwesomeIcon from '@fontawesome/fontawesome-free'
 import Restaurants from './restaurants.json'
+import UserProvider from './provider/UserProvider';
 export default class App extends Component {
   render() {
     return (
-      < BrowserRouter >
-        <div className="app">
-          <Route
-            render={({ location }) => {
-              return (
-                <Switch>
-                  <Route exact path='/' render={routerProps => {
-                    return <HomePage {...routerProps} Restaurants={Restaurants} />
-                  }
-                  } />
-                  <Route path={`/:id`} render={routerProps => {
-                    return <ResultRestaurant {...routerProps} Restaurants={Restaurants} />
-                  }
-                  } />
-                </Switch>
-              );
-            }}
-          />
-        </div>
-      </BrowserRouter >
+      <UserProvider>
+        <BrowserRouter >
+          <div className="app">
+            <Route
+              render={({ location }) => {
+                return (
+                  <Switch>
+                    <Route exact path='/' render={routerProps => {
+                      return <HomePage {...routerProps} Restaurants={Restaurants} />
+                    }
+                    } />
+                    <Route path={`/:id`} render={routerProps => {
+                      return <ResultRestaurant {...routerProps} Restaurants={Restaurants} />
+                    }
+                    } />
+                  </Switch>
+                );
+              }}
+            />
+          </div>
+        </BrowserRouter >
+      </UserProvider>
     )
   }
 }
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,46 +1,47 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import MapBox from './MapBox';
+import UserProvider from '../provider/UserProvider';
+import UserContext from '../provider/UserContext';
+
 export default class HomePage extends Component {
     constructor(props) {
         super(props)
-        this.newUserCoords = {
-            latitude: 0,
-            longitude: 0
-        }
-        this.getUserPosition()
     }
     componentDidMount() {
     }
-
-    getUserPosition() {
-        navigator.geolocation.getCurrentPosition(position => {
-            this.newUserCoords = {
-                latitude: position.coords.latitude,
-                longitude: position.coords.longitude,
-            }
-            console.log(position)
-        })
-    }
-    render() {
+    shuffleRestaurant = () => {
+        // get Restaurant
         const { Restaurants } = this.props.Restaurants
         let rand = Restaurants[Math.floor(Math.random() * Restaurants.length)];
 
+        //get Position
+        navigator.geolocation.getCurrentPosition(position => {
+            this.setState({ latitude: position.coords.latitude, longitude: position.coords.longitude })
+        })
+
+        // change page
+    }
+    render() {
         return (
-            <div className="container__home">
-                <div className="container__home--logo">
-                    <div className="container__home--logo-imgContainer">
-                        <img src="https://res.cloudinary.com/dgxpb4jhs/image/upload/v1573822753/feedmeat-logo_yebqdz.png"></img>
+            <UserContext.Consumer>
+                {({ latitude, longitude }) => (
+                    <div className="container__home">
+                        <NavLink to={{ pathname: `/:id` }}>
+                            <div className="container__home--logo">
+                                <div className="container__home--logo-imgContainer">
+                                    <img src="https://res.cloudinary.com/dgxpb4jhs/image/upload/v1573822753/feedmeat-logo_yebqdz.png"></img>
+                                </div>
+                            </div>
+                            <div className="container__home--shuffle">
+                                <div className="container__home--shuffle-content">
+                                    Cliquez sur l'écran pour découvrir un bon restaurant à moins de 800 mètres
+                            </div>
+                            </div>
+                        </NavLink>
                     </div>
-                </div>
-                <NavLink to={{ pathname: `/1` }}>
-                    <div className="container__home--shuffle">
-                        <div className="container__home--shuffle-content">
-                            Cliquez sur l'écran pour découvrir un bon restaurant à moins de 800 mètres
-                        </div>
-                    </div>
-                </NavLink>
-            </div>
+                )}
+            </UserContext.Consumer>
         )
     }
 }
