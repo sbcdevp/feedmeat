be6008e1f55c334d5940041884ba505bcb4f8b42
Sergio335566 <bocanceaserge335566@gmail.com>
2019-11-19 16:40:40 +0100

    resolve conflict

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,10 +3,7 @@ import { NavLink } from 'react-router-dom';
 import MapBox from './MapBox';
 import UserProvider from '../provider/UserProvider';
 import UserContext from '../provider/UserContext';
-<<<<<<< HEAD
 import Shuffle from './Shuffle';
-=======
->>>>>>> 6dfe38b6950811c35e179fab3f02ee1fdbc57d84
 
 export default class HomePage extends Component {
     constructor(props) {
@@ -16,13 +13,6 @@ export default class HomePage extends Component {
         this.shuffleComponent = new Shuffle()
         return this.shuffleComponent.getOneRestaurant()
     }
-    shuffleRestaurant = () => {
-        // get Restaurant
-        const { Restaurants } = this.props.Restaurants
-        let rand = Restaurants[Math.floor(Math.random() * Restaurants.length)];
-
-<<<<<<< HEAD
-
     render() {
         return (
             <div className="container__home">
@@ -36,35 +26,10 @@ export default class HomePage extends Component {
                         <div className="container__home--shuffle-content">
                             Cliquez sur l'écran pour découvrir un bon restaurant à moins de 800 mètres
                             </div>
-=======
-        //get Position
-        navigator.geolocation.getCurrentPosition(position => {
-            this.setState({ latitude: position.coords.latitude, longitude: position.coords.longitude })
-        })
-
-        // change page
-    }
-    render() {
-        return (
-            <UserContext.Consumer>
-                {({ latitude, longitude }) => (
-                    <div className="container__home">
-                        <NavLink to={{ pathname: `/:id` }}>
-                            <div className="container__home--logo">
-                                <div className="container__home--logo-imgContainer">
-                                    <img src="https://res.cloudinary.com/dgxpb4jhs/image/upload/v1573822753/feedmeat-logo_yebqdz.png"></img>
-                                </div>
-                            </div>
-                            <div className="container__home--shuffle">
-                                <div className="container__home--shuffle-content">
-                                    Cliquez sur l'écran pour découvrir un bon restaurant à moins de 800 mètres
-                            </div>
-                            </div>
-                        </NavLink>
->>>>>>> 6dfe38b6950811c35e179fab3f02ee1fdbc57d84
                     </div>
-                )}
-            </UserContext.Consumer>
+                    )}
+                </NavLink>
+            </div>
         )
     }
 }
